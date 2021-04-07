e5497beac01c4427201925f26b9afda6f2d5c056
Ivan Shevyrev <ivanvs140@yandex.ru>
2021-04-07 21:43:45 +0300

    counter test

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,9 @@
 fn main() {
     let source_string = "foobar";
-    let my_vec: Vec<char> = source_string.chars().collect();
-    println!("my_vec[0]: {}", my_vec[0]);
-    /* for i in my_vec {
-        println!("{}", i);
-    } */
-    let mut index = 0;
-    let mut string = String::new();
+    println!("{}", source_string);
 
-    while index < my_vec.len() {
-        println!("{} < indexing", my_vec[index]);
-        if my_vec[index] == my_vec[index + 1] {
-            string.push(my_vec[index]);
-        }
-        if index != 0 {
-            if my_vec[index] == my_vec[index - 1] {
-                string.push(my_vec[index]);
-            }
-        }
-        index += 1;
-        println!("{}", string);
+    for i in source_string.chars() {
+        println!("{}", i);
     }
-    println!("{}", string);
+
 }
